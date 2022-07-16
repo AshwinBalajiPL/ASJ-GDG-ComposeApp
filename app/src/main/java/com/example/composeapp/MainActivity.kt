d152df63aafe54c507f732d4174d0290fbe33739
Ashwin Balaji PL <79792605+AshwinBalajiPL@users.noreply.github.com>
2022-07-16 13:16:50 +0530

    add func to ui

--- app/src/main/java/com/example/composeapp/MainActivity.kt
+++ app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -5,10 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composeapp.ui.theme.ComposeAppTheme
@@ -23,35 +28,74 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxWidth(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Column() {
-                        Text(modifier = Modifier.padding(40.dp,10.dp),text = "Login", fontSize = 28.sp, fontWeight = FontWeight.Bold)
+                    Column(modifier = Modifier.fillMaxSize()) {
+                        Text(
+                            modifier = Modifier.padding(40.dp, 10.dp),
+                            text = "Login",
+                            fontSize = 28.sp,
+                            fontWeight = FontWeight.Bold
+                        )
                         Column(
-                            modifier = Modifier.padding(50.dp,0.dp),
+                            modifier = Modifier.padding(50.dp, 0.dp),
                         ) {
-                            var userName by remember{ mutableStateOf("")}
-                            var password by remember{ mutableStateOf("")}
+                            var userName by remember { mutableStateOf("") }
+                            var password by remember { mutableStateOf("") }
+                            var showPassword by remember { mutableStateOf(false) }
+                            var isUserNameError by remember {
+                                mutableStateOf(false)
+                            }
                             OutlinedTextField(
                                 value = userName,
-                                onValueChange = {userName = it},
-                                label = {Text("Username")},
+                                onValueChange = {
+                                    isUserNameError = it.contains(" ")
+                                    userName = it
+                                },
+                                label = { Text("Username") },
+                                isError = isUserNameError,
+                                singleLine = true,
                                 modifier = Modifier.padding(bottom = 10.dp)
                             )
                             OutlinedTextField(
                                 value = password,
-                                onValueChange = {password = it},
-                                label = {Text("Password")},
-                                modifier = Modifier.padding(top = 10.dp)
+                                onValueChange = { password = it },
+                                label = { Text("Password") },
+                                singleLine = true,
+                                visualTransformation = if (!showPassword) PasswordVisualTransformation(
+                                    '*'
+                                ) else VisualTransformation.None,
+                                modifier = Modifier.padding(top = 10.dp),
+                                trailingIcon = {
+                                    IconButton(onClick = {
+                                        showPassword = !showPassword
+                                    }) {
+                                        if (!showPassword)
+                                            Icon(
+                                                painter = painterResource(id = R.drawable.ic_lock),
+                                                contentDescription = "Hide"
+                                            )
+                                        else
+                                            Icon(
+                                                painter = painterResource(id = R.drawable.ic_lock_open),
+                                                contentDescription = "Show"
+                                            )
+                                    }
+                                }
                             )
-                            Button(onClick = { /*TODO*/ }, modifier = Modifier
-                                .padding(40.dp, 25.dp)
-                                .width(200.dp)
-                                .height(50.dp)) {
+                            Button(
+                                onClick = { /*TODO*/ }, modifier = Modifier
+                                    .padding(40.dp, 25.dp)
+                                    .width(200.dp)
+                                    .height(50.dp)
+                            ) {
                                 Text(text = "Login ", fontSize = 20.sp)
-                                Icon(painter = painterResource(id = R.drawable.ic_login), contentDescription = "Login")
+                                Icon(
+                                    painter = painterResource(id = R.drawable.ic_login),
+                                    contentDescription = "Login"
+                                )
                             }
                         }
                     }
-                    }
+                }
             }
         }
     }
